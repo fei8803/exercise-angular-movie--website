b1f65c931545e83c447f8d2377bd0d955b0d45eb
fei8803 <dsheffina@gmail.com>
2022-05-02 16:54:45 +0700

    1 binaries uploaded

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,10 +4,14 @@ import { FormsModule } from '@angular/forms';
 
 import { AppComponent } from './app.component';
 import { HelloComponent } from './hello.component';
+import { MovieComponent } from './movie/movie.component';
+import { LoginComponent } from './login/login.component';
+import { HomeComponent } from './home/home.component';
+import { HeaderComponent } from './header/header.component';
 
 @NgModule({
   imports:      [ BrowserModule, FormsModule ],
-  declarations: [ AppComponent, HelloComponent ],
+  declarations: [ AppComponent, HelloComponent, MovieComponent,LoginComponent,HomeComponent,HeaderComponent ],
   bootstrap:    [ AppComponent ]
 })
 export class AppModule { }
